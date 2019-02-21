6574f6aedbde5daed51bd691d0936f4b79e0c21c
Parinya <parinya@Parinyas-MacBook-Pro.local>
2019-02-21 16:47:12 +0700

    Updates

---
+++ precache-manifest.1c88b41f198bcb77b3c3d92a62da0cda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7befb03feb96bea94ec2",
+    "url": "/work-divider/static/js/runtime~main.7befb03f.js"
+  },
+  {
+    "revision": "1b5f24e20bd3ca7d1f43",
+    "url": "/work-divider/static/js/main.1b5f24e2.chunk.js"
+  },
+  {
+    "revision": "e65d2ef46aa8be294d01",
+    "url": "/work-divider/static/js/2.e65d2ef4.chunk.js"
+  },
+  {
+    "revision": "1b5f24e20bd3ca7d1f43",
+    "url": "/work-divider/static/css/main.68bb6c5d.chunk.css"
+  },
+  {
+    "revision": "193ac5a3f33595164337dcdac59457fb",
+    "url": "/work-divider/index.html"
+  }
+];
